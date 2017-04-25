1ec45385872039ca65cc54f6caa065088017239c
Maxime Martens <martenm@MacBook-Pro-Max.local>
2017-04-26 01:05:51 +0200

    Changed font, Fixed scrolling.

--- include/Commands.hh
+++ include/Commands.hh
@@ -13,13 +13,13 @@ namespace Commands {
   std::vector<std::string> *getArgs(std::string &command);
   bool parseCmd(Console &c, Engine &e, std::string);
   
-  void consoleClear(Console &c, Engine &e, std::vector<std::string> &);
-  void consoleToggle(Console &c, Engine &e, std::vector<std::string> &);
-  void execute(Console &c, Engine &e, std::vector<std::string> &);
-  void findCmd(Console &c, Engine &e, std::vector<std::string> &);
-  void quit(Console &c, Engine &e, std::vector<std::string> &);
-  void setMaxFPS(Console &c, Engine &e, std::vector<std::string> &);
-  void windowSize(Console &c, Engine &e, std::vector<std::string> &);
+  void consoleClear(Console &c, Engine &e, std::vector<std::string> *);
+  void consoleToggle(Console &c, Engine &e, std::vector<std::string> *);
+  void execute(Console &c, Engine &e, std::vector<std::string> *);
+  void findCmd(Console &c, Engine &e, std::vector<std::string> *);
+  void quit(Console &c, Engine &e, std::vector<std::string> *);
+  void setMaxFPS(Console &c, Engine &e, std::vector<std::string> *);
+  void windowSize(Console &c, Engine &e, std::vector<std::string> *);
   
 }
 
