eef8ddf78c22287722bfbea805f682fab731ab3f
Vault <contact@maximemartens.fr>
2017-07-11 00:33:44 +0200

    re

--- src/Resolver.cpp
+++ src/Resolver.cpp
@@ -2,4 +2,5 @@
 
 template <>
 std::unordered_map<std::string, sf::Font *> Resolver<sf::Font>::resources = std::unordered_map<std::string, sf::Font *>();
+template <>
 std::unordered_map<std::string, sf::Texture *> Resolver<sf::Texture>::resources = std::unordered_map<std::string, sf::Texture *>();
