946a55082819b718eebceef6f648a9f6037897a2
Maxime MARTENS KLEE <maxime.martens-ext@kleegroup.com>
2017-10-11 15:18:25 +0200

    Added GUIXML Parser placeholder

---
+++ include/GUIXML.hh
@@ -0,0 +1,19 @@
+/*!
+* @file      GUIXML.hh
+* @brief     GUI XML Parser
+*
+*            This classes set provide GUI elements loaders from XML Screens resources.
+*            It is used merely for building screens, but also to map GUIElements inside menus.
+*/
+#ifndef GUIXML_HH_
+#define GUIXML_HH_
+
+class GUIXML
+{
+public:
+
+private:
+
+};
+
+#endif /* !GUIXML_HH_ */
---
+++ src/GUIXML.cpp
@@ -0,0 +1,1 @@
+#include "GUIXML.hh"
