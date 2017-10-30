12e46e73c813b8f686c32d60341f6b2d612dff0e
Maxime MARTENS KLEE <maxime.martens-ext@kleegroup.com>
2017-10-30 11:36:04 +0100

    Added GUIElementGrid Placeholder

---
+++ src/GUIElementGrid.cpp
@@ -0,0 +1,62 @@
+#include "GUIElementGrid.hh"
+
+using namespace stb;
+
+GUIElementGrid::GUIElementGrid(const sf::Vector2i &size)
+{
+	_size = size;
+	_spacing = 0;
+}
+
+GUIElementGrid::~GUIElementGrid()
+{
+
+}
+
+void GUIElementGrid::initialUpdate()
+{
+
+}
+
+const sf::Vector2f GUIElementGrid::getPosition()
+{
+	if (_elements.size() > 0)
+		return (_elements[0]->getPosition());
+	return (sf::Vector2f(0, 0));
+}
+
+const sf::Vector2f GUIElementGrid::getSize()
+{
+	//tmp
+}
+
+void GUIElementGrid::pushElement(GUIElement *element)
+{
+	_elements.push_back(element);
+}
+
+void GUIElementGrid::setSpacing(int spacing)
+{
+	_spacing = spacing;
+}
+
+void GUIElementGrid::setPosition(const sf::Vector2f &pos)
+{
+	//tmp
+}
+
+bool GUIElementGrid::update(const sf::Event &e)
+{
+	for (size_t i = 0; i < _elements.size(); i++)
+	{
+		_elements[i]->update(e);
+	}
+}
+
+void GUIElementGrid::draw(sf::RenderWindow *win)
+{
+	for (size_t i = 0; i < _elements.size(); i++)
+	{
+		_elements[i]->draw(win);
+	}
+}
