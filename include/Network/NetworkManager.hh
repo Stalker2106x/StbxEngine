20df1cdfc720cd525cc9ec08b05a64de57bab014
Maxime MARTENS KLEE <maxime.martens-ext@kleegroup.com>
2018-01-24 18:10:18 +0100

    implemented basic NetworkManager; DOES NOT INSTANCIATE SUBCLASSES

--- include/Network/NetworkManager.hh
+++ include/Network/NetworkManager.hh
@@ -9,14 +9,28 @@ namespace stb {
 		//
 		// Client
 		//
-		void startClient()
+		void connect(const std::string &ip)
 		{
-
+			if (client != nullptr)
+			{
+				client->disconnect();
+				client = nullptr;
+			}
+			//client = std::make_shared<Client>(self); // INSTANCIATE NEW CLIENT TEMPLATED
+			if (client->connectServer(ip) == false) //if it fails, destroy client and redirect
+			{
+				client = nullptr;
+				//Connection failed!!!
+			}
 		}
 
-		void stopClient()
+		void disconnect()
 		{
-
+			if (client != nullptr) //Client already connected
+			{
+				client->disconnect();
+				client = nullptr;
+			}
 		}
 
 
@@ -25,12 +39,20 @@ namespace stb {
 		//
 		void startServer()
 		{
-
+			//server = std::make_shared<Server>(name, atoi(port.c_str()), slots); //INSTANCIATE TEMPLATED SERVER
+			if (server == nullptr)
+			{
+				//Server failed to start!!!
+			}
 		}
 
 		void stopServer()
 		{
-
+			if (server != nullptr) //Server is started
+			{
+				server->shutdown();
+				server = nullptr;
+			}
 		}
 
 		TServer server;
